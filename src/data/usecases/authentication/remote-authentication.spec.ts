1ab485f160ea05ebe4ca1c01f6c015329639222f
Jordi Sabat <jsabat@gmail.com>
2020-11-16 15:52:11 +0100

    refactor: change HttpPostClient to receive an object

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,15 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import faker from 'faker'
+import { HttpPostParams } from 'data/protocols/http/http-post-client'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = faker.internet.url()): SutTypes => {
+const makeSut = (params: HttpPostParams): SutTypes => {
+  const url = params.url
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
@@ -20,7 +22,7 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     // arrange
     const url = faker.internet.url()
-    const { sut, httpPostClientSpy } = makeSut(url)
+    const { sut, httpPostClientSpy } = makeSut({ url: url })
     // act
     await sut.auth()
     // assert
