2c8798c831f0556e43a275c069b1dc9ab2d1f8c3
Jordi Sabat <jsabat@gmail.com>
2020-11-16 17:09:06 +0100

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,25 @@
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import faker from 'faker'
-import { HttpPostParams } from 'data/protocols/http/http-post-client'
+// import { HttpPostParams } from 'data/protocols/http/http-post-client'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (params: HttpPostParams): SutTypes => {
-  const url = params.url
+// const makeSut = (params: HttpPostParams): SutTypes => {
+//   const url = params.url
+//   const httpPostClientSpy = new HttpPostClientSpy()
+//   const sut = new RemoteAuthentication(url, httpPostClientSpy)
+//   return {
+//     sut,
+//     httpPostClientSpy
+//   }
+// }
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
@@ -22,10 +32,17 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     // arrange
     const url = faker.internet.url()
-    const { sut, httpPostClientSpy } = makeSut({ url: url })
+    const { sut, httpPostClientSpy } = makeSut(url)
     // act
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     // assert
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const authenticationParams = mockAuthentication()
+    await sut.auth(authenticationParams)
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
 })
