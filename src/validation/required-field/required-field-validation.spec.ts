e98dd3ddbdf18bf4dd3891f44b7c1b43e3bae437
Jordi Sabat <jsabat@gmail.com>
2021-10-28 20:51:02 +0200

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -0,0 +1,13 @@
+import { RequiredFieldError } from '../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('RequiredFieldValidation', () => {
+  test('Should return error if field is empty', () => {
+    // arrange
+    const sut = new RequiredFieldValidation('email')
+    // act
+    const error = sut.validate('')
+    // assert
+    expect(error).toEqual(new RequiredFieldError())
+  })
+})
---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldError } from '../errors'
+import { FieldValidation } from '../protocolos/field-validation'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor(readonly field: string) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
+  validate(value: string): Error {
+    return new RequiredFieldError()
+  }
+}
