1d62f6565c29607710b37f7444530b048c41c0ee
Jordi Sabat <jsabat@gmail.com>
2021-11-15 18:33:49 +0100

    feat: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-compiste/validation-composite.spec.ts
+++ src/validation/validators/validation-compiste/validation-composite.spec.ts
@@ -5,23 +5,10 @@ describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     // arrange
     const fieldValidationSpy1 = new FieldValidationSpy('any_field')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_message')
-    const sut = new ValidationComposite([
-      fieldValidationSpy1,
-      fieldValidationSpy2
-    ])
-    // act
-    const error = sut.validate('any_field', 'any_value')
-    // assert
-    expect(error).toBe('any_message')
-  })
+    fieldValidationSpy1.error = new Error('first_error_message')
 
-  test('Should return error if any validation fails', () => {
-    // arrange
-    const fieldValidationSpy1 = new FieldValidationSpy('any_field')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_message')
+    fieldValidationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
       fieldValidationSpy1,
       fieldValidationSpy2
@@ -29,6 +16,6 @@ describe('ValidationComposite', () => {
     // act
     const error = sut.validate('any_field', 'any_value')
     // assert
-    expect(error).toBe('any_message')
+    expect(error).toBe('first_error_message')
   })
 })
