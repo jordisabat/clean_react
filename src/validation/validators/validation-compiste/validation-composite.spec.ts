76237ee0bf69ffaa26f75c83e33a56e191bbed09
Jordi Sabat <jsabat@gmail.com>
2021-11-15 18:38:48 +0100

    refactor: move SUT creation to a factory method

--- src/validation/validators/validation-compiste/validation-composite.spec.ts
+++ src/validation/validators/validation-compiste/validation-composite.spec.ts
@@ -1,18 +1,32 @@
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+
+  const sut = new ValidationComposite(fieldValidationsSpy)
+
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     // arrange
-    const fieldValidationSpy1 = new FieldValidationSpy('any_field')
-    fieldValidationSpy1.error = new Error('first_error_message')
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[0].error = new Error('first_error_message')
+    fieldValidationsSpy[1].error = new Error('second_error_message')
 
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('second_error_message')
-    const sut = new ValidationComposite([
-      fieldValidationSpy1,
-      fieldValidationSpy2
-    ])
     // act
     const error = sut.validate('any_field', 'any_value')
     // assert
